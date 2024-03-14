08418d6c67c031cdb419fe4fede0c95af6bdd1c7
Christopher Michael Chandler <christopher.chandler@outlook.de>
2024-03-14 11:32:05 +0100

    housecleaning

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import argparse
 # Custom
 from srt_processor.convertor import convert_file
 
-my_parser = argparse.ArgumentParser(description="Convert SRT to CSV>")
+my_parser = argparse.ArgumentParser(description="Convert SRT to CSV")
 
 my_parser.add_argument(
     "--f", "--file", type=str, help="the name of the file that should be converted."
